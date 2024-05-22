9722534b766f64ec9022cfdc0cb4c86d391c8158
Iasmim Siqueira <iasmimsas@gmail.com>
2024-05-22 17:57:23 -0300

    Dominio de Mercado e Exceptions

--- src/main/java/com/ifpb/CadastroDasEmpresasReguladas/controller/DominioDeMercadoController.java
+++ src/main/java/com/ifpb/CadastroDasEmpresasReguladas/controller/DominioDeMercadoController.java
@@ -1,22 +1,36 @@
 package com.ifpb.CadastroDasEmpresasReguladas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.ifpb.CadastroDasEmpresasReguladas.service.DominioDeMercadoService;
+import com.ifpb.CadastroDasEmpresasReguladas.model.DominioDeMercado;
+import com.ifpb.CadastroDasEmpresasReguladas.repository.DominioDeMercadoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/v1/dominioDeMercado")
 public class DominioDeMercadoController {
 
     @Autowired
-    private DominioDeMercadoService dominioDeMercadoService;
+    private DominioDeMercadoRepository dominioDeMercadoRepository;
 
-    // @GetMapping("/")
-    // public String findAll() {
-    //     List<DominioDeMercado> listaDominios = 
+    @GetMapping()
+    public ResponseEntity<List<DominioDeMercado>> findAll() {
+        List<DominioDeMercado> listaDominios = dominioDeMercadoRepository.findAll();
+        return ResponseEntity.ok(listaDominios);
         
-    // }
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<DominioDeMercado> getById(@PathVariable Integer codigo) {
+        DominioDeMercado dominioDeMercado = dominioDeMercadoRepository.findById(codigo);
+        return ResponseEntity.ok(dominioDeMercado);
+    }
+    
 
 }
