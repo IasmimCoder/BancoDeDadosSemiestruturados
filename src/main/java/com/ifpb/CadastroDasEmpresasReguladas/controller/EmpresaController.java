d2bfaeac39b0947805e7b6ed64e405b3a0c31ac8
Iasmim Siqueira <iasmimsas@gmail.com>
2024-05-24 16:23:29 -0300

    Endpoint Empresa

--- src/main/java/com/ifpb/CadastroDasEmpresasReguladas/controller/EmpresaController.java
+++ src/main/java/com/ifpb/CadastroDasEmpresasReguladas/controller/EmpresaController.java
@@ -1,26 +1,56 @@
 package com.ifpb.CadastroDasEmpresasReguladas.controller;
 
 import com.ifpb.CadastroDasEmpresasReguladas.model.Empresa;
-import com.ifpb.CadastroDasEmpresasReguladas.repository.EmpresaRepository;
+import com.ifpb.CadastroDasEmpresasReguladas.service.EmpresaService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/empresa")
+@CrossOrigin(origins = "*", maxAge = 3600) //gera uma fila
+@RequestMapping("/api/v1/empresa")
 public class EmpresaController {
 
     @Autowired
-    EmpresaRepository empresaRepository;
+    EmpresaService empresaService;
 
     @PostMapping
-    public ResponseEntity<Object> saveEmpresa(@RequestBody Empresa empresa) {
-//        if(empresaRepository.existsByEntNome(empresa.getEntnome())) {
-//            return ResponseEntity.status(HttpStatus.CONFLICT).body("Já existe uma empresa cadastrada com esse nome!");
-//        }
+    public ResponseEntity<Empresa> saveEmpresa(@RequestBody Empresa empresa) {
+        Empresa empresaCriada = empresaService.save(empresa);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(empresaRepository.save(empresa));
+        return ResponseEntity.status(HttpStatus.CREATED).body(empresaCriada);
     }
+
+    @GetMapping()
+    public ResponseEntity<List<Empresa>> findAll() {
+        List<Empresa> listaDeEmpresas = empresaService.findAll();
+        return ResponseEntity.ok(listaDeEmpresas);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Empresa> findById(@RequestParam String codigo) {
+        Empresa empresa = empresaService.findById(codigo);
+        return ResponseEntity.ok(empresa);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Empresa> update(@PathVariable String codigo, @RequestBody Empresa empresa) {
+        Empresa empresaAtualizada = empresaService.update(codigo, empresa);
+        return ResponseEntity.ok(empresaAtualizada);
+    }
+    
+    @DeleteMapping
+    public void deleteById(@PathVariable String codigo){
+        empresaService.deleteById(codigo);
+    }
+    
 }
---
+++ src/main/java/com/ifpb/CadastroDasEmpresasReguladas/service/EmpresaService.java
@@ -0,0 +1,51 @@
+package com.ifpb.CadastroDasEmpresasReguladas.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.ifpb.CadastroDasEmpresasReguladas.exceptions.ExistingEntityException;
+import com.ifpb.CadastroDasEmpresasReguladas.exceptions.NotFoundException;
+import com.ifpb.CadastroDasEmpresasReguladas.model.Empresa;
+import com.ifpb.CadastroDasEmpresasReguladas.repository.EmpresaRepository;
+
+public class EmpresaService {
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+    public Empresa save(Empresa empresa) {
+        
+       if(empresaRepository.existsByEntNome(empresa.getEntnome())) {
+        throw new ExistingEntityException("Já existe uma empresa cadastrada com esse nome!");
+       }
+       
+       return empresaRepository.save(empresa);
+    }
+
+    public List<Empresa> findAll() {
+        return empresaRepository.findAll();
+    }
+
+    public Empresa findById(String codigo) {
+       return empresaRepository.findById(codigo).orElseThrow(
+        () -> new NotFoundException("Empresa não encontrada com o CNPJ: " + codigo)
+       );
+    }
+
+    public Empresa update(String codigo, Empresa empresa) {
+       Empresa empresaAtualizada = findById(codigo);
+    
+       empresaAtualizada.setEntcodigofip(empresa.getEntcodigofip());
+       empresaAtualizada.setDataautorizacao(empresa.getDataautorizacao());
+       empresaAtualizada.setEntnome(empresa.getEntnome());
+
+
+       return save(empresaAtualizada);
+    }
+
+    public void deleteById(String codigo) {
+       this.findById(codigo);
+       empresaRepository.deleteById(codigo);
+    }
+
+}
