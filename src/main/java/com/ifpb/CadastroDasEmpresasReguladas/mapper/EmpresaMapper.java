91ab836b2f5f753bedfde3eef0786b6df45738ef
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2024-06-03 17:25:48 -0300

    Problemas corrigidos no EmpresaMapper

--- src/main/java/com/ifpb/CadastroDasEmpresasReguladas/mapper/EmpresaMapper.java
+++ src/main/java/com/ifpb/CadastroDasEmpresasReguladas/mapper/EmpresaMapper.java
@@ -3,6 +3,9 @@ package com.ifpb.CadastroDasEmpresasReguladas.mapper;
 import com.ifpb.CadastroDasEmpresasReguladas.model.*;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Component
 public class EmpresaMapper {
 
@@ -20,6 +23,7 @@ public class EmpresaMapper {
         empresa.setEntcodigofip(empresaDTO.getEntcodigofip());
         empresa.setEntnome(empresaDTO.getEntnome());
         empresa.setEntcgc(empresaDTO.getEntcgc());
+        empresa.setDataautorizacao(empresaDTO.getDataautorizacao());
 
         return empresa;
     }
@@ -28,23 +32,22 @@ public class EmpresaMapper {
         //retira todos os caracteres não-numéricos (incluindo espaço,tab, etc)
        telefone = telefone.replaceAll("\\D","");
 
-       //verifica se o numero foi digitado com todos os dígitos iguais
-       java.util.regex.Pattern p = java.util.regex.Pattern.compile(telefone.charAt(0)+"{"+telefone.length()+"}");
-       java.util.regex.Matcher m = p.matcher(telefone);
-       if(m.find()) return false;
-
-
         if (telefone.startsWith("0800")) {
-            
+
             //verifica se tem a qtde de numeros correta
             if (telefone.length() == 11){
                 return true;
-            } 
+            }
             return false;
         }
 
         //verifica se tem a qtde de numeros correta
-       if (!(telefone.length() >= 8 && telefone.length() <= 10)) return false;
+        if (!(telefone.length() >= 8 && telefone.length() <= 10)) return false;
+
+       //verifica se o numero foi digitado com todos os dígitos iguais
+       java.util.regex.Pattern p = java.util.regex.Pattern.compile(telefone.charAt(0)+"{"+telefone.length()+"}");
+       java.util.regex.Matcher m = p.matcher(telefone);
+       if(m.find()) return false;
 
        return true;
     }
@@ -71,8 +74,30 @@ public class EmpresaMapper {
        return true;
     }
 
+    public static boolean verificarDDD(String ddd){
+        
+        String[] DDDs = {
+                "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                "21", "22", "24", "27", "28", "31", "32", "33", "34",
+                "35", "37", "38", "41", "42", "43", "44", "45", "46",
+                "47", "48", "49", "51", "53", "54", "55", "61", "62",
+                "64", "63", "65", "66", "67", "68", "69", "71", "73",
+                "74", "75", "77", "79", "81", "82", "83", "84", "85",
+                "86", "87", "88", "89", "91", "92", "93", "94", "95",
+                "96", "97", "98", "99"};
+
+        for(String d: DDDs){
+            if(ddd.equals(d)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static Contato verNullContato(Contato contato){
-        if (contato.getDdd()==null||contato.getDdd().equals("0")){
+        if (contato.getDdd()==null){
+            contato.setDdd("Não registrado!");
+        } else if (!verificarDDD(contato.getDdd())) {
             contato.setDdd("Não registrado!");
         }
 
@@ -83,9 +108,10 @@ public class EmpresaMapper {
             contato.setFax("Não registrado!");
         }
         
-        if (contato.getTelefone()==null){
+        if (contato.getTelefone()==null||contato.getTelefone().isEmpty()){
             contato.setTelefone("Não registrado!");
-        }else if (!validarTelefone(contato.getTelefone())){
+        }
+        else if (!validarTelefone(contato.getTelefone())){
             contato.setTelefone("Não registrado!");
         }
         
@@ -114,9 +140,6 @@ public class EmpresaMapper {
         }
         if (empresaDTO.getEntcgc()==null||empresaDTO.getEntcgc().equals("00000000000000")){
             empresaDTO.setEntcgc("Não registrado!");
-        }
-        if(empresaDTO.getDataautorizacao()==null){
-            empresaDTO.setDataautorizacao("Não registrado!");
         }
         return empresaDTO;
     }
